02ce40a1bdf0464a16218fe03f6f18ad1550c537
Orlahmide <tomilolaaturaka@gmail.com>
2025-03-10 06:40:05 +0100

    Add files via upload

---
+++ StackOOP/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stack = new StackProperties();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop()); // 3
+            Console.WriteLine(stack.Pop()); // 2
+            Console.WriteLine(stack.Pop()); // 1
+
+
+            //Console.WriteLine(stack.Pop()); This line is used to simulate the part that throws an exception
+
+            
+
+
+        }
+    }
+}
